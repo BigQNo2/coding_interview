327644404c10c400ee7e05514841b26d89bd46e8
BigQNo2 <leeygx@bu.edu>
2014-10-07 17:09:59 -0400

    Ready for adding two data structures of stack and queue implemented by Linked List

---
+++ coding_interview_book/python_solution/queue.py
@@ -0,0 +1,9 @@
+"""A module that contains the class to implement the data structure of queue
+"""
+
+from linkedlist import linkedListNode
+
+class Queue(object):
+    """docstring for Queue"""
+    def __init__(self, arg):
+        self.arg = arg
---
+++ coding_interview_book/python_solution/stack.py
@@ -0,0 +1,10 @@
+"""A module that contains the class to implement the data structure of stack
+"""
+
+from linkedlist import linkedListNode
+
+class Stack(object):
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        self.arg = arg
+ 
