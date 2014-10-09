94b36871433e89b0ecb3c0a1fdc03b1825904de6
BigQNo2 <leeygx@bu.edu>
2014-10-09 12:45:33 -0400

    Added the Classes of stack and queue implemented by linkedlist

--- coding_interview_book/python_solution/queue.py
+++ coding_interview_book/python_solution/queue.py
@@ -1,9 +1,31 @@
 """A module that contains the class to implement the data structure of queue
 """
 
-from linkedlist import linkedListNode
+from linkedlist import LinkedListNode
 
 class Queue(object):
-    """docstring for Queue"""
-    def __init__(self, arg):
-        self.arg = arg
+    """the linkedList implementation. Adding tail and Removing head."""
+
+    def __init__(self):
+        self.tail = None
+        self.head = None
+
+    def enqueue(self, data):
+        """Add the data to the queue tail."""
+        new_tail = LinkedListNode(data)
+        if self.tail == None:
+            self.tail = new_tail
+            self.head = new_tail
+        else:
+            self.tail.next = new_tail
+            self.tail = new_tail #Update the tail
+
+    def dequeue(self):
+        """Remove the data from queue head"""
+        if self.head != None:
+            item = self.head.data
+            self.head = self.head.next #Update the head
+
+            return item
+        else:
+            return None
--- coding_interview_book/python_solution/stack.py
+++ coding_interview_book/python_solution/stack.py
@@ -1,10 +1,35 @@
 """A module that contains the class to implement the data structure of stack
 """
 
-from linkedlist import linkedListNode
+from linkedlist import LinkedListNode
 
 class Stack(object):
-    """docstring for ClassName"""
-    def __init__(self, arg):
-        self.arg = arg
+    """the LinkedList implementation by adding or removing LinkedList head"""
+
+    def __init__(self):
+        self.top = None 
+
+    def push(self, data):
+        """Push data to the stack top. Add new head.
+        """
+        new_top = LinkedListNode(data)
+        new_top.next = self.top
+        self.top = new_top
+
+    def pop(self):
+        """Pop data from the stack top. Remove current head.
+        """
+        if self.top != None:
+            item = self.top.data
+            self.top = self.top.next
+
+            return item
+        else:
+            return None
+
+    def peek(self):
+        """Peek the stack top"""
+        return self.top.data
+
+
  
